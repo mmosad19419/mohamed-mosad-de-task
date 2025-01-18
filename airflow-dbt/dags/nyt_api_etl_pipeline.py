354453591a5eb0f0353e6fdf6324378bf1e13bd4
Mohamed Mosaad <mohamed.mosaad@elmenus.com>
2025-01-18 20:58:48 +0200

    add airflow dag for data ingestion

--- airflow-dbt/dags/nyt_api_etl_pipeline.py
+++ airflow-dbt/dags/nyt_api_etl_pipeline.py
@@ -7,8 +7,6 @@ from airflow.operators.python import PythonOperator
 from airflow.utils.dates import days_ago
 from airflow.providers.postgres.hooks.postgres import PostgresHook
 
-
-
 # Import helper functions
 from plugins.helper_functions import parse_configs, generate_incremental_dates, fetch_data_from_api, \
     init_db_connection, read_json_file, transform_data, load_data, write_rejected_records_to_file, \
@@ -22,11 +20,12 @@ START_DATE = '2020-12-27'
 END_DATE = '2023-12-31'
 OFFSET = 7
 
- # Fetch Data from API
+# Fetch Data from API
 def fetch_data_function():
     # Generate the list of dates for incremental loading
     dates = generate_incremental_dates(START_DATE, END_DATE, OFFSET)
 
+    # make api calls for each date
     for date in dates:
         try:
             logger.info(f"Starting fetch for date {date}")
@@ -42,6 +41,7 @@ def process_data_function(date):
     # Generate the list of dates for incremental loading
     dates = generate_incremental_dates(START_DATE, END_DATE, OFFSET)
 
+     # process raw data files for each date
     for date in dates:
         try:
             logger.info(f"Starting data processing for {date}")
@@ -67,7 +67,7 @@ def process_data_function(date):
     cursor.close()
     conn.close()
 
-# Task 3: Validate Data
+# Validate Data ingested date range
 def validate_data_task():
     try:
         logger.info("Starting data validation")
@@ -80,7 +80,8 @@ def validate_data_task():
         logger.error(f"Error during data validation: {e}")
         raise
 
-# Airflow DAG definition
+
+# Define the DAG
 with DAG(
     'nyt_books_etl_pipeline',
     default_args={
@@ -89,12 +90,12 @@ with DAG(
         'retries': 1,
         'retry_delay': timedelta(minutes=5),
     },
-    schedule_interval='@weekly',  # Adjust according to your needs
+    schedule_interval='@weekly',
     catchup=False,
     tags=['etl', 'books', 'nyt']
 ) as dag:
 
-    # Set up logging for the DAG
+    # Set up logging
     logger = logging.getLogger('airflow.task')
 
     fetch_data = validate_data = PythonOperator(
@@ -113,6 +114,6 @@ with DAG(
         python_callable=validate_data_task
     )
 
-    # Set up tasks order
+    # Set up tasks dependiencies
     fetch_data >> process_data
     process_data >> validate_data
