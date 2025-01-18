d3b4d1dd0f2178e9510ba2517a9421e260a4d876
Mohamed Mosaad <mohamed.mosaad@elmenus.com>
2025-01-18 20:55:21 +0200

    add airflow dag for data ingestion

---
+++ airflow-dbt/dags/nyt_api_etl_pipeline.py
@@ -0,0 +1,118 @@
+import logging
+from datetime import datetime, timedelta
+import time
+
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from airflow.utils.dates import days_ago
+from airflow.providers.postgres.hooks.postgres import PostgresHook
+
+
+
+# Import helper functions
+from plugins.helper_functions import parse_configs, generate_incremental_dates, fetch_data_from_api, \
+    init_db_connection, read_json_file, transform_data, load_data, write_rejected_records_to_file, \
+    validate_published_dates
+
+# Fetching the API Credentials
+NYT_BOOKS_API_KEY, NYT_BOOKS_API_SECRET, NYT_BOOKS_API_ENDPOINT = parse_configs('./config/config.json')
+
+# Define the start and end dates for the range
+START_DATE = '2020-12-27'
+END_DATE = '2023-12-31'
+OFFSET = 7
+
+ # Fetch Data from API
+def fetch_data_function():
+    # Generate the list of dates for incremental loading
+    dates = generate_incremental_dates(START_DATE, END_DATE, OFFSET)
+
+    for date in dates:
+        try:
+            logger.info(f"Starting fetch for date {date}")
+            fetch_data_from_api(NYT_BOOKS_API_KEY, date)
+            logger.info(f"Successfully fetched data for date {date}")
+            time.sleep(12)  # Sleep for 12 seconds to avoid hitting API rate limits
+        except Exception as e:
+            logger.error(f"Error fetching data for date {date}: {e}")
+            raise
+
+# Task 2: Process Data
+def process_data_function(date):
+    # Generate the list of dates for incremental loading
+    dates = generate_incremental_dates(START_DATE, END_DATE, OFFSET)
+
+    for date in dates:
+        try:
+            logger.info(f"Starting data processing for {date}")
+            # Read the raw data
+            data = read_json_file(f'./raw_data/{date}.json')
+
+            # Transform the data
+            df_lists, df_books, df_buy_links, df_best_sellers, rejected_records = transform_data(data)
+
+            # Load data into the database
+            conn, cursor = init_db_connection("localhost", "mydb", "admin", "admin", "5432")
+            load_data(conn, cursor, df_lists, df_books, df_buy_links, df_best_sellers)
+
+            # Handle rejected records
+            write_rejected_records_to_file(rejected_records)
+
+            logger.info(f"Successfully processed data for {date}")
+        except Exception as e:
+            logger.error(f"Error processing data for {date}: {e}")
+            raise
+    
+    # Close the DB connection
+    cursor.close()
+    conn.close()
+
+# Task 3: Validate Data
+def validate_data_task():
+    try:
+        logger.info("Starting data validation")
+        conn, cursor = init_db_connection("localhost", "mydb", "admin", "admin", "5432")
+        validate_published_dates(cursor, START_DATE, END_DATE)
+        cursor.close()
+        conn.close()
+        logger.info("Data validation completed successfully")
+    except Exception as e:
+        logger.error(f"Error during data validation: {e}")
+        raise
+
+# Airflow DAG definition
+with DAG(
+    'nyt_books_etl_pipeline',
+    default_args={
+        'owner': 'mmosad19419@gmail.com',
+        'start_date': days_ago(1),
+        'retries': 1,
+        'retry_delay': timedelta(minutes=5),
+    },
+    schedule_interval='@weekly',  # Adjust according to your needs
+    catchup=False,
+    tags=['etl', 'books', 'nyt']
+) as dag:
+
+    # Set up logging for the DAG
+    logger = logging.getLogger('airflow.task')
+
+    fetch_data = validate_data = PythonOperator(
+        task_id='fetch_data',
+        python_callable=fetch_data_function
+    )
+
+
+    process_data = validate_data = PythonOperator(
+        task_id='process_data',
+        python_callable=process_data_function
+    )
+
+    validate_data = PythonOperator(
+        task_id='validate_data',
+        python_callable=validate_data_task
+    )
+
+    # Set up tasks order
+    fetch_data >> process_data
+    process_data >> validate_data
---
+++ airflow-dbt/tests/test_helper_functions.py
@@ -0,0 +1,64 @@
+import os
+import json
+import pytest
+from datetime import datetime
+import requests
+from unittest.mock import patch, MagicMock
+
+from plugins.helper_functions import parse_configs, generate_incremental_dates, fetch_data_from_api
+
+# Unit test generate_incremental_dates
+def test_generate_incremental_dates_with_large_offset():
+    start_date = "2023-01-01"
+    end_date = "2023-01-10"
+    offset = 7
+    expected_dates = [
+        datetime(2023, 1, 1).date(),
+        datetime(2023, 1, 8).date()
+    ]
+    dates = generate_incremental_dates(start_date, end_date, offset)
+    assert dates == expected_dates
+
+def test_generate_incremental_dates_no_dates():
+    start_date = "2023-01-01"
+    end_date = "2022-12-31"
+    offset = 1
+    dates = generate_incremental_dates(start_date, end_date, offset)
+    assert dates == []
+
+
+# Test fetch_data_from_api function
+@patch('requests.get')
+def test_fetch_data_from_api_success(mock_get):
+    # get API credientials
+    NYT_BOOKS_API_KEY, NYT_BOOKS_API_SECRET, NYT_BOOKS_API_ENDPOINT = parse_configs('./config/config.json')
+
+    # Mock the response from the API
+    mock_response = MagicMock()
+    mock_response.status_code = 200
+    mock_response.json.return_value = {"status": "OK", "results": {"lists": []}}
+    
+    # Set the mock to return the above response when called
+    mock_get.return_value = mock_response
+
+    DATE = "2023-01-01"
+    
+    # Call the actual function
+    fetch_data_from_api(NYT_BOOKS_API_KEY, DATE)
+
+    # Check that requests.get was called with the expected URL
+    mock_get.assert_called_once_with(
+        f"https://api.nytimes.com/svc/books/v3/lists/overview.json?published_date={DATE}&api-key={NYT_BOOKS_API_KEY}"
+    )
+
+def test_fetch_data_from_api_failure():
+    # Create a mock for requests.get simulating a Notfound error
+    mock_get = MagicMock()
+    mock_get.return_value.status = 404
+
+    with MagicMock() as requests_mock:
+        requests_mock.get = mock_get
+        NYT_BOOKS_API_KEY = "test_api_key"
+        DATE = "2023-01-01"
+        with pytest.raises(Exception):
+            fetch_data_from_api(NYT_BOOKS_API_KEY, DATE)
